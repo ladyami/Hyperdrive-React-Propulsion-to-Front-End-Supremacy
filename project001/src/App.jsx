9c58badfe44750e3ccc7f280a9d2c65797874864
Amina Oyegoke <99187115+ladyami@users.noreply.github.com>
2024-01-08 22:24:53 +0100

    folder structures

--- project001/src/App.jsx
+++ project001/src/App.jsx
@@ -1,13 +1,18 @@
 //import { useState } from 'react'
-
 import './App.css'
+import Nav from './Navigation/Nav';
+import Products from './Products/Products';
+import Recommended from './Recommended/Recommended'
+
 
 function App() {
   
   return (
     <>
-      <h1>hello world</h1>
-      
+    <Nav/>
+    <Products/>
+    <Recommended />
+
     </>
   )
 }
